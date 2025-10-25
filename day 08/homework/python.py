93a59402fa1a76a2c1e6b2d5f25e593357f479b3
Alexandre Linniki <alexandrelinniki@gmail.com>
2025-10-25 21:30:18 +0400

    level day 08 homework

---
+++ day 08/homework/python.py
@@ -0,0 +1,17 @@
+print("1) keyword")
+print("2) saitis cvladi")
+print("3) integer")
+print("keyword - dasaweri nebismieri sityva, romelic aucileblad unda chaiweros")
+print("saitis cvladia, romelic iwereba mistvis")
+print("integer aris igive ricxvi")
+
+print("for aris keyword, romelic aucileblad unda chaiweros")
+
+for i in range(1001):
+    print(i)
+
+for i in range(15):
+    print("xinkali")
+
+for i in range(5):
+    print("Alexandre,Lile,Naira,Levani,Vova,Maia")
