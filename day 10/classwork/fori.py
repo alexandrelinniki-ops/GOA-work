d65d40723453864d14feb488def6258f9a7ba5c9
Alexandre Linniki <alexandrelinniki@gmail.com>
2025-11-01 21:19:34 +0400

    level day 10

---
+++ day 10/classwork/fori.py
@@ -0,0 +1,11 @@
+name="Alexadre"
+for i in name:
+    print("Alexandre")
+
+age=13
+for i in str(age):
+    print(i)
+
+correctPassword="Hello123"
+guess=input("Please enter your password")
+
---
+++ day 10/homework/while.py
@@ -0,0 +1,31 @@
+x = 1
+
+while x <= 20:
+    print(x)
+    x = x * 2
+
+counter = 5
+
+while counter == 20:
+    print(counter)
+    counter = counter + 3
+
+text = ""
+
+while len(text) < 6:
+    text = text + "*"
+    print(text)
+
+number = 1
+
+for i in range(1,6):
+    number *= i
+    print(sum(i))
+    print(type(int(i)))
+
+product = 1
+
+for i in range(1,5):
+    product *= i
+    print(product)
+
