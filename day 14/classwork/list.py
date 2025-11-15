5b9a310324c5d4e375e06fceaf97c53859b4c7ac
Alexandre Linniki <alexandrelinniki@gmail.com>
2025-11-15 23:13:27 +0400

    level day 14

---
+++ day 14/classwork/list.py
@@ -0,0 +1,15 @@
+print("indexshi atvla yoveltvis iwyeba 0, radgan compiuteri 0 iwyeba atvla")
+
+family =["Alexandre","Lile","Naira","Levani","Vova","Maia"]
+print(family)
+
+#mutable - monacemta tipis shesvla shesadzlebelia"("List")
+#immutable - monacemta tipis shesvla sheudzlebelia"("Boolean,Integer,String,Float")
+
+family = ["Alexandre","Lile","Naira","Levani","Vova","Maia"]
+print(family[0])
+print(family[1])
+print(family[2])
+print(family[3])
+print(family[4])
+print(family[5])
---
+++ day 14/homework/isag.py
@@ -0,0 +1,27 @@
+#mutable monacemta tipis shesvla sheudzlebelia(Integer,String,Boolean,Float)
+#immutable monacemta tipis shecvla shesadzlebelia(List)
+
+family = ["Alexandre","Lile","Naira","Levani","Vova","Maia"]
+ages = [13,5,40,38,62,60]
+
+print(family)
+print(ages)
+
+fruits =["pear","orange","banana","watermelon"]
+fruits[0] = "I love apple"
+
+print(fruits[0])
+
+colors =["red","yellow","green","blue"]
+
+print(colors[0])
+print(colors[2])
+
+words =["GOA","at","I","study"]
+
+print(words[2], words[3], words[1], words[0])
+print(words[-2], words[-1], words[-3], words[-4])
+
+
+
+      
