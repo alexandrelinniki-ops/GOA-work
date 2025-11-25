1894dfa0071ee4b084382e65be23a14d10007dd6
Alexandre Linniki <alexandrelinniki@gmail.com>
2025-11-25 18:08:30 +0400

    commit

---
+++ day 17/classwork/lists.py
@@ -0,0 +1,11 @@
+print("slicing stringze mushaobs")
+
+name = "Gurami"
+
+print(name[1:4])
+
+family =["Alexandre","Lile","Naira","Levani","Vova","Maia"]
+
+print(family[0:2])
+print(family[2:4])
+print(family[4:6])
---
+++ day 17/homework/slicing.py
@@ -0,0 +1,24 @@
+print("slicingi gamoaqvs indexinidan ramdenime lists")
+
+cars = ["BMW","Mercedes","Ferrari","Bugati","Toyota","Lamborgini","Subaru"]
+
+print(cars[3:5])
+
+name = input("Enter your name:")
+
+print(name[1:-1])
+
+name1 = "Alexandre"
+
+names = ["Alexandre","Ilia","Vaja","Gaga","Tato","Daviti"]
+
+print(name1[0:4])
+print(names[0:3])
+
+name2 = input("Enter your name:")
+
+if name2[0] == "r":
+    
+    print("Hello")
+else:
+    print("Bye")
