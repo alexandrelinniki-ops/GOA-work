24f479f3e414e87c1e8baa569c92fca64e4f2974
Alexandre Linniki <alexandrelinniki@gmail.com>
2026-02-07 20:33:50 +0400

    commit

---
+++ Day 039/classwork/python.py
@@ -0,0 +1,13 @@
+# 0) კომენტარებით ახსენით რას აკეთებს order და align-self
+# 1) დაასრულეთ flexbox froggy
+# 2) ჩამოწერეთ ყველა position და ახსენით რას აკეთებენ ისინი
+# 3) შექმენით საიტი, სადაც ერთ-ერთი ელემენტი იქნება ეკრანის მარჯვნივ ზემოთ კუთხეში, ეკრანის ჩამოწევის შემთხვევაშიც
+
+# 0)
+# order - gansazrvravs elementis tanmimdevrobas
+# align-self - sashualebas gvadzlevs ert elements mivcet sakutari argumenti
+
+# 1)
+
+# image.png
+
